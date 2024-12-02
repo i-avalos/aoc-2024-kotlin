6b88644846c70a42e52edcb3c54a390b4d832cf2
Ignacio Avalos San Martin <iavalos@squareup.com>
2024-12-02 21:24:39 +0100

    day 02. not very inspired today!

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,17 +1,70 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int = input.size
+    fun part1(input: List<String>): Int {
+        val lists = input.map { it.split(" ") }
+        var failures = 0
 
-    fun part2(input: List<String>): Int = input.size
+        for (list in lists) {
+            for (i in list.indices) {
+                val value = list[i].toInt()
+                val next = list.getOrNull(i + 1)?.toInt()
+                val previous = list.getOrNull(i - 1)?.toInt()
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+                if (previous != null && next != null) {
+                    if ((previous > value && next > value) || (previous < value && next < value)) {
+                        failures++
+                        break
+                    }
+                }
+
+                if (next != null && (abs(value - next) > 3 || abs(value - next) == 0)) {
+                    failures++
+                    break
+                }
+            }
+        }
+
+        return input.size - failures
+    }
+
+    fun part2(input: List<String>): Int {
+        val lists = input.map { it.split(" ") }
+        var successes = 0
+
+        for (list in lists) {
+            outer@ for (i in list.indices) {
+                val modifiedList = list.toMutableList().apply { removeAt(i) }
+
+                for (j in modifiedList.indices) {
+                    val value = modifiedList[j].toInt()
+                    val next = modifiedList.getOrNull(j + 1)?.toInt()
+                    val previous = modifiedList.getOrNull(j - 1)?.toInt()
+
+                    if (previous != null && next != null) {
+                        if ((previous > value && next > value) || (previous < value && next < value)) {
+                            continue@outer
+                        }
+                    }
+
+                    if (next != null && (abs(value - next) > 3 || abs(value - next) == 0)) {
+                        continue@outer
+                    }
+                }
+                successes++
+                break@outer
+            }
+        }
+
+        return successes
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test")
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
 }
